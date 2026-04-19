ac9ed57c5e2666359ed20636cb576f21650c5599
Navneet <navneet0077@gmail.com>
2026-04-20 01:12:11 +0530

    Added solution including MaxDistance

---
+++ MaxDistance.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1855 lang=java
+ *
+ * [1855] Maximum Distance Between a Pair of Values
+ */
+
+// 
+class MaxDistance{
+    public int maxDist(int[] nums1,int[] nums2){
+        int i=0,j=0,res=0;
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]>nums2[j]){
+                i++;
+
+            }else{
+                res=Math.max(res,j-i);
+                j++;
+            }
+        }
+        return res;
+    }
+    
+}
+
+
+// @lc code=end
+
