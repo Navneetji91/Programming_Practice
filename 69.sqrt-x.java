cd8b58b70482438f239cd5ce266da7102541ed2d
Navneet <navneet0077@gmail.com>
2026-04-22 00:00:48 +0530

    Add LeetCode 69: Sqrt(x) - Binary Search Solution

---
+++ 69.sqrt-x.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=69 lang=java
+ *
+ * [69] Sqrt(x)
+ */
+
+// @lc code=start
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+
+        int left = 1, right = x / 2;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long square = (long) mid * mid;
+
+            if (square == x) {
+                return mid;
+            } else if (square < x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return right;        
+    }
+}
+// @lc code=end
+
